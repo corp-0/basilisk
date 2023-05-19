a60504838164407cec94167d4ce58ad68c28f0a1
Andrés Franco Riquelme Sandoval <andresfranco.rs@gmail.com>
2023-05-18 22:53:08 -0400

    add debug ui and insanity manager

---
+++ Src/Autoloads/InsanityManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Godot;
+using MonoCustomResourceRegistry;
+
+namespace Basilisk.Autoloads;
+
+[RegisteredType(nameof(InsanityManager))]
+public partial class InsanityManager: Node
+{
+    private int CurrentInsanity { get; set; }
+    [Export] private int DefaultInsanityPenalty { get; set; }
+    [Export] private int MaxInsanity { get; set; }
+    private int CurrentInsanityPercent => (CurrentInsanity / MaxInsanity) * 100;
+    
+    //current amount, current amount in percentage
+    public event Action<int, int>? InsanityChanged;
+    
+    public void AddInsanity(int? amount = null)
+    {
+        CurrentInsanity += amount ?? DefaultInsanityPenalty;
+        if (CurrentInsanity > MaxInsanity)
+            CurrentInsanity = MaxInsanity;
+        InsanityChanged?.Invoke(CurrentInsanity, CurrentInsanity / CurrentInsanityPercent);
+    }
+    
+    public void ResetInsanity()
+    {
+        CurrentInsanity = 0;
+        InsanityChanged?.Invoke(CurrentInsanity, CurrentInsanity / CurrentInsanityPercent);
+    }
+}
---
+++ Src/Scripts/Ui/DebugUi.cs
@@ -0,0 +1,31 @@
+﻿using Basilisk.Autoloads;
+using Chickensoft.GoDotNet;
+using Godot;
+using MonoCustomResourceRegistry;
+
+namespace Basilisk.Ui;
+
+[RegisteredType(nameof(DebugUi), baseType: nameof(Control))]
+public partial class DebugUi: Control
+{
+    private InsanityManager InsanityManager => this.Autoload<InsanityManager>();
+    [Export] private Label Insanity { get; set; } = null!;
+    [Export] private Label InsanityPercent { get; set; } = null!;
+    
+    public override void _Ready()
+    {
+        InsanityManager.InsanityChanged += OnInsanityChanged;
+    }
+
+    private void OnInsanityChanged(int lvl, int percent)
+    {
+        Insanity.Text = lvl.ToString();
+        InsanityPercent.Text = $"{percent}%";
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event.IsActionPressed("toggle_debug_ui"))
+            Visible = !Visible;
+    }
+}
